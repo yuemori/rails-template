c76220abc94419cb15061ef5c3c8d17b9cdbd494
yuemori <yuemori@aiming-inc.com>
2017-03-21 01:06:39 +0900

    Fix template uri

--- setup.sh
+++ setup.sh
@@ -10,6 +10,6 @@ echo "gem 'rails'" >> Gemfile
 
 bundle install --path vendor/bundle --binstubs vendor/bundle/bin -j 4
 
-vendor/bundle/bin/rails new . --skip-bundle --skip-test --force --template=../rails-templates/template.rb
+vendor/bundle/bin/rails new . --skip-bundle --skip-test --force --template=https://raw.githubusercontent.com/yuemori/rails-template/master/template.rb
 
 bundle install --path vendor/bundle --binstubs vendor/bundle/bin --jobs=4

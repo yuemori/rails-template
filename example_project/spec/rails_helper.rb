24dd9d107cdf7b04a92d1ed54da1de60c0f3eb45
yuemori <yuemori@aiming-inc.com>
2017-04-19 00:00:00 +0900

    Update example_project

--- example_project/spec/rails_helper.rb
+++ example_project/spec/rails_helper.rb
@@ -1,16 +1,24 @@
-require "simplecov"
+require 'simplecov'
 
-SimpleCov.start "rails"
+SimpleCov.start 'rails'
 
-ENV["RAILS_ENV"] ||= "test"
+ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path("../../config/environment", __FILE__)
-require "spec_helper"
-require "rspec/rails"
+require File.expand_path('../../config/environment', __FILE__)
+require 'spec_helper'
+require 'rspec/rails'
+require 'turnip_helper'
 
-Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 RSpec.configure do |config|
   config.fixture_path = Rails.root.join('spec', 'fixtures')
@@ -20,37 +28,15 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
 
-  config.before(:suite) do
-    if config.use_transactional_fixtures?
-      raise(<<-MSG)
-        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
-        (or set it to false) to prevent uncommitted transactions being used in
-        JavaScript-dependent specs.
-        During testing, the app-under-test that the browser driver connects to
-        uses a different database connection to the database connection used by
-        the spec. The app's database connection would not be able to access
-        uncommitted transaction data setup over the spec's database connection.
-      MSG
-    end
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before :each do
-    DatabaseCleaner.strategy = :transaction
-  end
-
   config.before :each, type: :feature do
-    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
-    if !driver_shares_db_connection_with_specs
-      DatabaseCleaner.strategy = :truncation
-    end
+    Capybara.current_driver == :rack_test
   end
 
-  config.before :each do
-    DatabaseCleaner.start
+  config.before :suite do
+    DatabaseRewinder.clean_all
   end
 
   config.after :each do
-    DatabaseCleaner.clean
+    DatabaseRewinder.clean
   end
 end

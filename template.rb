b57a9d8572277f986c6a2aacd9a55cef371af3ba
yuemori <yuemori@aiming-inc.com>
2017-04-18 23:35:52 +0900

    cosme

--- template.rb
+++ template.rb
@@ -356,18 +356,18 @@ end
 RUBY
 
 create_file 'spec/rails_helper.rb', <<RUBY, force: true
-require "simplecov"
+require 'simplecov'
 
-SimpleCov.start "rails"
+SimpleCov.start 'rails'
 
-ENV["RAILS_ENV"] ||= "test"
+ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path("../../config/environment", __FILE__)
-require "spec_helper"
-require "rspec/rails"
-require "turnip_helper"
+require File.expand_path('../../config/environment', __FILE__)
+require 'spec_helper'
+require 'rspec/rails'
+require 'turnip_helper'
 
-Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
